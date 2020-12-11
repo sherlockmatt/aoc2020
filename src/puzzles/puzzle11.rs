521ea53fd7d40ced65a7aa62c5e360d58488616b
sherlockmatt <matthew.w.dawson@gmail.com>
2020-12-11 15:24:32 +0000

    Day 11 tweak

--- src/puzzles/puzzle11.rs
+++ src/puzzles/puzzle11.rs
@@ -6,7 +6,7 @@ pub fn run(input: String) -> Vec<String> {
     let parts: Vec<(for<'r, 's> fn(&'r Pos, usize, usize, &'s HashMap<Pos, bool>) -> Vec<Pos>, usize)> = vec![(neighbouring_seats, 4usize), (visible_seats, 5usize)];
 
     for (seat_fn, min_chairs) in parts {
-        let mut grid: HashMap<Pos, bool> = HashMap::new();//input.lines().enumerate().flat_map(|(y, l)| l.chars().enumerate().map(|(x, c)| (Pos { x:x.clone(), y:y.clone() }, c == '#'))).collect();
+        let mut grid: HashMap<Pos, bool> = HashMap::new();
         for (y, l) in input.lines().enumerate() {
             for (x, c) in l.chars().enumerate() {
                 if c != '.' {
@@ -17,7 +17,7 @@ pub fn run(input: String) -> Vec<String> {
         let Pos { x: max_x, y: max_y } = grid.keys().map(|&p| p).max().unwrap();
 
         loop {
-            let mut new_grid: HashMap<Pos, bool> = HashMap::new();
+            let mut new_grid: HashMap<Pos, bool> = grid.clone();
             for (p, s) in grid.clone() {
                 new_grid.insert(p, match (s, seat_fn(&p, max_x, max_y, &grid).iter().filter(|p| grid.contains_key(p) && *grid.get(p).unwrap()).count()) {
                     (false, 0) => true,
@@ -36,12 +36,16 @@ pub fn run(input: String) -> Vec<String> {
     return answers;
 }
 
+lazy_static! {
+    static ref OFFSETS: [(i128, i128); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+}
+
 fn neighbouring_seats(p: &Pos, max_x: usize, max_y: usize, _: &HashMap<Pos, bool>) -> Vec<Pos> {
     let mut seats = Vec::new();
-    for (dx, dy) in vec![(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)] {
-        if (dx >= 0 || p.x > 0) && (dx <= 0 || p.x < max_x) &&
-            (dy >= 0 || p.y > 0) && (dy <= 0 || p.y < max_y) {
-            seats.push(Pos { x: (p.x as i128 + dx) as usize, y: (p.y as i128 + dy) as usize });
+    for (dx, dy) in OFFSETS.iter() {
+        if (*dx >= 0 || p.x > 0) && (*dx <= 0 || p.x < max_x) &&
+            (*dy >= 0 || p.y > 0) && (*dy <= 0 || p.y < max_y) {
+            seats.push(Pos { x: (p.x as i128 + *dx) as usize, y: (p.y as i128 + *dy) as usize });
         }
     }
     seats
@@ -49,11 +53,11 @@ fn neighbouring_seats(p: &Pos, max_x: usize, max_y: usize, _: &HashMap<Pos, bool
 
 fn visible_seats(p: &Pos, max_x: usize, max_y: usize, grid: &HashMap<Pos, bool>) -> Vec<Pos> {
     let mut seats = Vec::new();
-    for (dx, dy) in vec![(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)] {
+    for (dx, dy) in OFFSETS.iter() {
         let mut cur_pos = p.clone();
-        while (dx >= 0 || cur_pos.x > 0) && (dx <= 0 || cur_pos.x < max_x) &&
-            (dy >= 0 || cur_pos.y > 0) && (dy <= 0 || cur_pos.y < max_y) {
-            cur_pos = Pos { x: (cur_pos.x as i128 + dx) as usize, y: (cur_pos.y as i128 + dy) as usize };
+        while (*dx >= 0 || cur_pos.x > 0) && (*dx <= 0 || cur_pos.x < max_x) &&
+            (*dy >= 0 || cur_pos.y > 0) && (*dy <= 0 || cur_pos.y < max_y) {
+            cur_pos = Pos { x: (cur_pos.x as i128 + *dx) as usize, y: (cur_pos.y as i128 + *dy) as usize };
             if grid.contains_key(&cur_pos) {
                 seats.push(cur_pos);
                 break;
